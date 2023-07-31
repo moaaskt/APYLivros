4d3bcf81047ba925e30cd39fa0d3a4a7186107b2
moaaskt <moacirmayer@hotmail.com>
2023-07-30 22:02:11 -0300

    criando mensagens para as rotas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 
@@ -23,19 +23,14 @@ livros = [
 
 @app.route('/livros', methods=['GET'])
 def obter_livros():
-    return jsonify(livros)
-
-
-# consultar id
+    return jsonify(livros), 200
 
 @app.route('/livros/<int:id>', methods=['GET'])
 def obter_livro_por_id(id):
     for livro in livros:
         if livro.get('id') == id:
-            return jsonify(livro)
-
-
-# editar livro por id
+            return jsonify(livro), 200
+    return jsonify({'mensagem': 'Livro não encontrado'}), 404
 
 @app.route('/livros/<int:id>', methods=['PUT'])
 def editar_livro_por_id(id):
@@ -43,26 +38,28 @@ def editar_livro_por_id(id):
     for indice, livro in enumerate(livros):
         if livro.get('id') == id:
             livros[indice].update(livro_alterado)
-            return jsonify(livros[indice])
-
-# criar livro
-
+            return jsonify(livros[indice]), 200
+    return jsonify({'mensagem': 'Livro não encontrado'}), 404
 
 @app.route('/livros', methods=['POST'])
 def criar_novo_livro():
     novo_livro = request.get_json()
+    if 'title' not in novo_livro or 'autor' not in novo_livro:
+        return jsonify({'mensagem': 'Campos title e autor são obrigatórios'}), 400
+
+    novo_livro['id'] = len(livros) + 1
     livros.append(novo_livro)
-
-    return jsonify(livros)
-
+    return jsonify({'mensagem': 'Livro Cadastrado com sucesso'}), 200
+    return jsonify(novo_livro), 201 
+    
 
 @app.route('/livros/<int:id>', methods=['DELETE'])
 def excluir_livro(id):
-   for indice, livro in enumerate(livros):
-    if livro.get('id') == id:
-        del livros[indice]
-
-        return jsonify(livros)
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+            return jsonify({'mensagem': 'Livro excluído com sucesso'}), 200
+    return jsonify({'mensagem': 'Livro não encontrado'}), 404
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, debug=True)
